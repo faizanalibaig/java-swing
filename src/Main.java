36059e9d9c887d1a332395a449a0e458418e7764
faizanalibaig <riazfiaz6181@gmail.com>
2024-09-11 21:07:50 +0500

    java frame

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,29 @@
+import javax.swing.*;
+import java.awt.*;
+
+class showroom extends JFrame{
+    public showroom(){
+        System.out.println("Hello");
+
+        JFrame frame = new JFrame("Calculator");
+        frame.setSize(500, 500);
+
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+        JPanel panel = new JPanel();
+        JButton button = new JButton("Click here");
+        button.setFont(new Font("Arial", Font.BOLD, 12));
+        button.setBackground(Color.BLACK);
+        panel.add(button);
+
+        frame.add(panel);
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello world");
+      showroom sr= new showroom();
     }
 }
