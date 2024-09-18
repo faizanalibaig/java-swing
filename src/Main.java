22c4b32dbaf9e7fae9f752360c491ce7e381072f
faizanalibaig <riazfiaz6181@gmail.com>
2024-09-18 08:42:32 +0500

    changes in main

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,4 @@
 import javax.swing.*;
-import java.awt.*;
-import javax.swing.ImageIcon;
-
-class showroom extends JFrame{
-    public showroom(){
-        JFrame frame= new JFrame("main application");
-
-
-        //basic frame settings
-        frame.setSize(400, 400);
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        frame.setLocationRelativeTo(null);
-        frame.setResizable(false);
-
-        ImageIcon image = new ImageIcon("./images.png");
-
-
-        //label
-        JLabel label= new JLabel("Hello World");
-
-        frame.add(label);
-        frame.setIconImage(image.getImage());
-    }
-}
 
 public class Main {
     public static void main(String[] args) {
